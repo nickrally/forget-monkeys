19bbfb7440cd8d81c115e8ee5bc731a34073c4bc
nickrally <skunk@pop-os.localdomain>
2022-02-05 00:32:06 -0700

    made monkeys homepage

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,12 @@
 import { Route, Switch } from "react-router-dom";
-import HomePage from "./home/HomePage";
-import Temp from "./temp/Temp";
-import Monkeys from "./forget/Monkeys";
+import Monkeys from "./home/Monkeys";
+import PageNotFound from "./common/PageNotFound";
 import React from 'react';
 const App = () => {
     return (
         <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/monkeys" component={Monkeys} />
-        <Route path="/temp" component={Temp} />
-        
+        <Route exact path="/" component={Monkeys} />
+        <Route component={PageNotFound} />
       </Switch>
     );
   }
--- src/components/temp/Temp.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-const Temp = () =>{
-    return (
-        <h3>Temp</h3>
-    )
-}
-export default Temp;
